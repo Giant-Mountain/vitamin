92bbb4a82c11f33979275d3b9f701f669403ba87
SmilePig <306826385@qq.com>
2019-10-14 10:26:16 +0800

    navList

---
+++ src/components/InputList/index.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="inputList">
+    <label><span>手机号:</span>
+      <el-input
+        v-model="input"
+        placeholder="请输入内容"
+      /></label>
+    <label><span>所属店铺:</span>
+      <el-select
+        v-model="value"
+        placeholder="请选择"
+      >
+        <el-option
+          v-for="item in options"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        />
+      </el-select>
+    </label>
+    <label><span>角色:</span>
+      <el-select
+        v-model="value"
+        placeholder="请选择"
+      >
+        <el-option
+          v-for="item in options"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        />
+      </el-select>
+    </label>
+    <label><span>顾客账号:</span>
+      <el-select
+        v-model="value"
+        placeholder="请选择"
+      >
+        <el-option
+          v-for="item in options"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        />
+      </el-select>
+    </label>
+    <label><span>状态:</span>
+      <el-select
+        v-model="value"
+        placeholder="请选择"
+      >
+        <el-option
+          v-for="item in options"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        />
+      </el-select>
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='scss' scoped>
+.inputList {
+    padding: 48px;
+    display: flex;
+    flex-wrap: wrap;
+    label {
+        width: 500px;
+        height: 80px;
+        line-height: 80px;
+        display: inline-block;
+        text-align: center;
+        .el-input,
+        .el-select {
+            width: 250px;
+        }
+        span {
+            width: 70px;
+            display: inline-block;
+            text-align: end;
+        }
+    }
+}
+</style>
+
---
+++ src/views/team/memberManger/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container">
+    <NavHeader
+      :title="title"
+      :nav-list="navList"
+      :current="current"
+      :flag="flag"
+      @handleClicks="clicks"
+    />
+    <div class="main">
+      <div v-show="current===0">
+        <InputList />
+      </div>
+      <div v-show="current===1">123234</div>
+      <div v-show="current===2">123235</div>
+    </div>
+  </div>
+</template>
+<script>
+import InputList from '@/components/InputList'
+import NavHeader from '@/components/NavHeader'
+export default {
+  name: 'MemberManger',
+  components: {
+    InputList,
+    NavHeader
+  },
+  data() {
+    return {
+      navList: ['员工管理', '邀请中', '角色描述'],
+      current: 0,
+      title: '员工管理',
+      flag: false
+    }
+  },
+  methods: {
+    clicks(current) {
+      this.current = current
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+%juzhong {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.container {
+    width: 100%;
+    height: auto;
+    color: rgba(0, 0, 0, 0.65);
+}
+</style>
+
